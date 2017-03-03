66570977c92f26ed37a0295aa7ef3b1d40abfd61
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2017-03-03 16:11:46 -0500

    .

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,12 @@ angular.module('MyApp')
   .controller('HomeCtrl', function($scope, Contact) {
     $scope.skills = {};
     $scope.calculate = function() {
-      return (5 * $scope.skills.height);
-    }
+      var tmb = 0;
+      if ($scope.skills.gender === 'm') {
+        tmb = (10 * $scope.skills.weight) + (6.25 * $scope.skills.height) - (5 * $scope.skills.age) - 161;
+      } else{
+        tmb = (10 * $scope.skills.weight) + (6.25 * $scope.skills.height) - (5 * $scope.skills.age) + 5;
+      }
+      return tmb * parseFloat($scope.skills.activity);
+    };
   });
--- public/js/application.js
+++ public/js/application.js
@@ -136,8 +136,14 @@ angular.module('MyApp')
   .controller('HomeCtrl', ["$scope", "Contact", function($scope, Contact) {
     $scope.skills = {};
     $scope.calculate = function() {
-      return (5 * $scope.skills.height);
-    }
+      var tmb = 0;
+      if ($scope.skills.gender === 'm') {
+        tmb = (10 * $scope.skills.weight) + (6.25 * $scope.skills.height) - (5 * $scope.skills.age) - 161;
+      } else{
+        tmb = (10 * $scope.skills.weight) + (6.25 * $scope.skills.height) - (5 * $scope.skills.age) + 5;
+      }
+      return tmb * parseFloat($scope.skills.activity);
+    };
   }]);
 angular.module('MyApp')
   .controller('LoginCtrl', ["$scope", "$rootScope", "$location", "$window", "$auth", function($scope, $rootScope, $location, $window, $auth) {
